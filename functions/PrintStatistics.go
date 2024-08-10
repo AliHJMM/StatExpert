ab76793b6221b5d76ddf7f9f822be832d9eb932b
Ali <139016234+AliHJMM@users.noreply.github.com>
2024-08-10 20:20:38 +0300

    Update PrintStatistics.go

--- functions/PrintStatistics.go
+++ functions/PrintStatistics.go
@@ -1,10 +1,10 @@
 package functions
 
 import "fmt"
-
+// PrintStatistics outputs the average, median, variance, and standard deviation.
 func PrintStatistics(average, median, variance, stddev float64) {
-	fmt.Printf("Average: %.0f\n", average)
-	fmt.Printf("Median: %.0f\n", median)
-	fmt.Printf("Variance: %.0f\n", variance)
-	fmt.Printf("Standard Deviation: %.0f\n", stddev)
+	fmt.Printf("Average: %.0f\n", average)  // Print the average
+	fmt.Printf("Median: %.0f\n", median)  // Print the median
+	fmt.Printf("Variance: %.0f\n", variance)  // Print the variance
+	fmt.Printf("Standard Deviation: %.0f\n", stddev)  // Print the standard deviation
 }
