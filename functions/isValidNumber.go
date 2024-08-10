16151abae8a01ff7fbbae439beb5773b54c2fd33
Ali <139016234+AliHJMM@users.noreply.github.com>
2024-08-10 20:18:17 +0300

    Validation comments

--- functions/isValidNumber.go
+++ functions/isValidNumber.go
@@ -1,8 +1,8 @@
 package functions
 
 import "regexp"
-
+// isValidNumber checks if a string contains only digits.
 func isValidNumber(s string) bool {
-	match, _ := regexp.MatchString(`^\d+$`, s)
+	match, _ := regexp.MatchString(`^\d+$`, s) // Use regex to check if the string is composed of digits only
 	return match
 }
