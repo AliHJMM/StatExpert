95e1a5e76f5b91fdfea5a19bb56c09e4ebcae780
Ali <AliHasanJMM@gmail.com>
2024-08-10 19:56:22 +0300

    Return Results

--- functions/stats.go
+++ functions/stats.go
@@ -19,13 +19,12 @@ func CalculateStatistics(array []int) (average, median, variance, stddev float64
 	} else {
 		median = math.Ceil(float64(array[len(array)/2]))
 	}
+
 	var varianceSum float64
 	for _, num := range array {
 		varianceSum += math.Pow((float64(num) - average), 2)
 	}
-
 	variance = math.Round(varianceSum / float64(len(array)))
-
 	stddev = math.Round(math.Sqrt(variance))
 
 	return average, median, variance, stddev
