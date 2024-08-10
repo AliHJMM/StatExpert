ce396eccb64b728b552834955319326b506c086c
Ali <AliHasanJMM@gmail.com>
2024-08-10 19:50:28 +0300

    main program

--- main.go
+++ main.go
@@ -12,4 +12,14 @@ func main(){
 	}
 
 	filename := os.Args[1]
+
+	array, err := ReadNumbersFromFile(filename)
+	if err != nil {
+		log.Fatalf("Error reading from file: %s", err)
+	}
+
+	if len(array) == 0 {
+		log.Println("No valid numbers to process.")
+		return
+	}
 }
