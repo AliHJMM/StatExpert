1c1906aee7954ae79605ab37031fc7182ed5aaf6
Ali <AliHasanJMM@gmail.com>
2024-08-10 19:51:57 +0300

    files  program

--- functions/files.go
+++ functions/files.go
@@ -9,5 +9,12 @@ import (
 )
 
 func ReadNumbersFromFile(filename string) ([]int, error) {
-	
+	var array []int
+	readingFile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer readingFile.Close()
+
+	file := bufio.NewScanner(readingFile)
 }
